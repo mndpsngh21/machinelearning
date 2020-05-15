179be23db9c4b41c5aaefda0a2c5246b55118c95
mndpsngh21 <mndpsngh21@gmail.com>
2020-05-15 23:27:32 +0530

    Upper confidence and Thompson Sampling practice

--- ReinforcementLearning/ThompsonSampling/thompson_sampling.py
+++ ReinforcementLearning/ThompsonSampling/thompson_sampling.py
@@ -1,4 +1,4 @@
-# Thompson Sampling
+#Practice code for Thompson Sampling 
 
 # Importing the libraries
 import numpy as np
@@ -9,27 +9,37 @@ import pandas as pd
 dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
 
 # Implementing Thompson Sampling
+# create initial reward matrics
 import random
 N = 10000
 d = 10
 ads_selected = []
+# fill initial data with zero
 numbers_of_rewards_1 = [0] * d
 numbers_of_rewards_0 = [0] * d
 total_reward = 0
+
 for n in range(0, N):
     ad = 0
     max_random = 0
     for i in range(0, d):
+        #implement formula for calculate beta value
         random_beta = random.betavariate(numbers_of_rewards_1[i] + 1, numbers_of_rewards_0[i] + 1)
+        # if value is more than max random then consider it as selection
         if random_beta > max_random:
             max_random = random_beta
+            # select which is performing better
             ad = i
+    #add selected ad to selection list        
     ads_selected.append(ad)
+    # check reward status
     reward = dataset.values[n, ad]
+    # of reward is positive add to positive otherwise to 0 reward
     if reward == 1:
         numbers_of_rewards_1[ad] = numbers_of_rewards_1[ad] + 1
     else:
         numbers_of_rewards_0[ad] = numbers_of_rewards_0[ad] + 1
+    # add reward value to total reward only positive rewards will be counted in total reward        
     total_reward = total_reward + reward
 
 # Visualising the results - Histogram
